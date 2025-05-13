a40636a2e88462efb889d107296d6ef9299798dc
Daniel Muñoz Paredes <110432268+DanielMunozParedes@users.noreply.github.com>
2025-05-12 22:49:01 -0500

    Update building_arp_request_3.1.py

--- Man in the middle attacks/ARP Spoofing/Scripts/building_arp_request_3.1.py
+++ Man in the middle attacks/ARP Spoofing/Scripts/building_arp_request_3.1.py
@@ -1,4 +1,3 @@
-
 from scapy.all import ARP, Ether ,sendp ,conf , get_if_hwaddr, get_if_addr, subprocess , sniff
 
 iface = conf.iface
@@ -14,15 +13,22 @@ def packet_handler(pkt):
             #REPLY
             if pkt[ARP].pdst == MyIp and pkt[ARP].psrc == requested:
                 subprocess.run(["sudo", "arp", "-s", pkt[ARP].psrc, pkt[ARP].hwsrc ])
-
+                print(f" + ARP REPLY SUCCESS FROM {pkt[ARP].psrc}  and updated in the ARP table of this host with the ip: {MyIp}") 
 
     arp_packet = Ether(src = MyMac , dst = "FF:FF:FF:FF:FF:FF")/ARP(op=1, pdst = requested , psrc = MyIp, hwsrc = MyMac)
-    sendp(arp_packet, iface=iface, count = 1, inter = 1.0)     
+    sendp(arp_packet, iface=iface, count = 1, inter = 1.0) 
+    print(f" + ARP REQUEST was sended to {requested} from this host with the ip: {MyIp}")    
 
 
 
 
 sniff(filter="arp", prn = packet_handler, count = 10)
+
+
+
+
+
+#sudo apr -d ip we want to delete from the table
 
 
 
